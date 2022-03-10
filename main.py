864de87e46a9b5a852c5d51d8edba2a025ab6c5e
Dung Ngo <dungngotanlhp@gmail.com>
2022-03-10 16:17:15 -0600

    deploy to flask

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from flask import request
 from flask_cors import CORS
-import asyncio
 
 from utilities.chatbot import read_file, find_similarity, answer
 from utilities.filewriter import findAnswer, newRead
@@ -9,18 +8,19 @@ from utilities.filewriter import findAnswer, newRead
 app = Flask(__name__)
 CORS(app)
 
+
 @app.route("/answer", methods=['POST'])
 def answer_endpoint():
     newRead()
-    
+
     questions = read_file("data/questions.txt")
     message = request.json['message']
-    
+
     ranks = find_similarity(questions, message)
     a_index = answer(ranks)
-    
+
     return findAnswer(a_index)
 
 
-if __name__ == '__main__':    
+if __name__ == '__main__':
     app.run(debug=True)
